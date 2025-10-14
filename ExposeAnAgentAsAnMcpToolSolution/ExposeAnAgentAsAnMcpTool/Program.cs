0d6276d1221edd478b49757edc6f1d16e4f70cd4
James McAliece <JMcAliece@mycorpaccess.com>
2025-10-14 13:47:03 -0700

    MCP -

---
+++ ExposeAnAgentAsAnMcpToolSolution/ExposeAnAgentAsAnMcpTool/Program.cs
@@ -0,0 +1,65 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Server;
+using OpenAI;
+using System;
+using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+Console.WriteLine("Starting...");
+var endpoint = LoadSettings();
+AIAgent agent = new AzureOpenAIClient(
+    new Uri(endpoint),
+    new DefaultAzureCredential())
+        .GetChatClient("gpt-4o")
+        .CreateAIAgent(instructions: "You are good at telling jokes.", name: "Joker");
+
+McpServerTool tool = McpServerTool.Create(agent.AsAIFunction());
+
+
+
+HostApplicationBuilder builder = Host.CreateEmptyApplicationBuilder(settings: null);
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools([tool]);
+
+await builder.Build().RunAsync();
+
+Console.WriteLine("Exiting...");
+
+static string LoadSettings()
+{
+    string filePath = "local.settings.json";
+
+    if (!File.Exists(filePath))
+    {
+        Console.WriteLine("local.settings.json not found.");
+        return string.Empty;
+    }
+
+    string jsonContent = File.ReadAllText(filePath);
+
+    using JsonDocument doc = JsonDocument.Parse(jsonContent);
+    JsonElement root = doc.RootElement;
+
+    if (root.TryGetProperty("Values", out JsonElement values))
+    {
+        Console.WriteLine("Loading Settings");
+        foreach (JsonProperty setting in values.EnumerateObject())
+        {
+            // Add each setting to the dictionary
+            if (setting.Name == "Endpoint")
+            {
+                return setting.Value.GetString() ?? string.Empty;
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("No 'Values' section found in local.settings.json.");
+    }
+
+    return string.Empty;
+}
